bd6771cbea200ea351ea75f0b983d60be3831e30
Jonas Almeida <jalmeida@mathbiol.org>
2021-05-15 12:17:44 -0400

    dependency on strgr removed

--- episphere.R
+++ episphere.R
@@ -11,7 +11,7 @@
 episphere <- function(){
   # ini
   # info on packaging at https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
-  episphere.require(c("boxr","stringr"))  # use collection for multiple libraries
+  episphere.require("boxr")  # use collection for multiple libraries
   box_auth("627lww8un9twnoa8f9rjvldf7kb56q1m","gSKdYKLd65aQpZGrq9x4QVUNnn5C8qqm")
 }
 
@@ -37,21 +37,17 @@ episphere.openFolder <- function(id=133596945131){
   message(paste("folder #",id," opened in your browser",sep=""))
 }
 
-episphere.openFile <- function(id=787813783402,ver=""){
-  if(stringr::str_length(ver)==0){
-    episphere.openURL(paste("https://nih.app.box.com/file/",id,sep=""))
-    message(paste("file #",id," opened in your browser",sep=""))
-  }else{
-    episphere.openURL(paste("https://nih.app.box.com/file/",id,"?sb=/details/versions/",ver,sep=""))
-    message(paste("file #",id," version #",ver," opened in your browser",sep=""))
+episphere.openFile <- function(id=787813783402,ver=NULL){
+  url=paste("https://nih.app.box.com/file/",id,sep="")
+  if(typeof(NULL)!="NULL"){
+    url=paste(url,"?sb=/details/versions/",ver,sep="")
   }
+  message(paste("browser tab opened at",url))
+  episphere.openURL(url)
   # for example of a specific version, episphere.openFile(787813783402,841936237402)
 }
 
-episphere.read <- function(id=787813783402,ver=""){
-  if(stringr::str_length(ver)==0){
-    ver=NULL
-  }
+episphere.read <- function(id=787813783402,ver=NULL){
   box_read(file_id=id,version_id=ver)
 }
 
