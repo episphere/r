3090165182fc38ccbc652511d590fa8ec2b735ad
Jonas Almeida <jalmeida@mathbiol.org>
2021-05-14 22:06:12 -0400

    message

--- episphere.R
+++ episphere.R
@@ -24,13 +24,16 @@ episphere.openURL <- function(url="https://episphere.github.io/r"){
 
 episphere.openFolder <- function(id=133596945131){
   episphere.openURL(paste("https://nih.app.box.com/folder/",id,sep=""))
+  message(paste("folder #",id," opened in your browser",sep=""))
 }
 
 episphere.openFile <- function(id=787813783402,ver=""){
   if(stringr::str_length(ver)==0){
     episphere.openURL(paste("https://nih.app.box.com/file/",id,sep=""))
+    message(paste("file #",id," opened in your browser",sep=""))
   }else{
     episphere.openURL(paste("https://nih.app.box.com/file/",id,"?sb=/details/versions/",ver,sep=""))
+    message(paste("file #",id," version #",ver," opened in your browser",sep=""))
   }
   # for example of a specific version, episphere.openFile(787813783402,841936237402)
 }
